e0230ae1729ee4953701506667d7f9205a69423e
Karlito14 <leiroz26@hotmail.com>
2024-03-06 14:11:07 +0100

    ajout fichier factoryMedias.js pour la gestion des medias

---
+++ scripts/templates/factoryMedias.js
@@ -0,0 +1,52 @@
+export class Factory {
+    constructor(path, title, type) {
+        switch(type) {
+            case 'image': 
+                return new Image(path, title);
+            case 'video':
+                return new Video(path, title);
+        }
+    }
+}
+
+class Image {
+    constructor(path, title) {
+        this.path = path;
+        this.title = title;
+    }
+
+    create() {
+        this.element = document.createElement('img');
+        this.element.setAttribute('alt', this.title);
+        this.element.setAttribute('src', this.path);
+        this.element.setAttribute('class', 'image-media');
+        this.element.setAttribute('aria-haspopup', 'dialog');
+        this.element.setAttribute('aria-controls', 'carousel_modal');
+        this.element.setAttribute('data-name', `${this.title}`);
+        return this.element;
+    }
+}
+
+class Video{
+    constructor(path, title) {
+        this.path = path;
+        this.title = title;
+    }
+
+    create() {
+        this.element = document.createElement('video');
+        this.element.setAttribute('alt', this.title);
+
+        this.error = document.createElement('span');
+        this.error.textContent = 'Votre navigateur ne prend pas en charge les vidéos HTML5';
+
+        this.element.appendChild(this.error);
+
+        this.element.setAttribute('src', this.path);
+        this.element.setAttribute('class', 'image-media');
+        this.element.setAttribute('aria-haspopup', 'dialog');
+        this.element.setAttribute('aria-controls', 'carousel_modal');
+        this.element.setAttribute('data-name', `${this.title}`);
+        return this.element;
+    }
+}
--- scripts/templates/medias.js
+++ scripts/templates/medias.js
@@ -1,3 +1,5 @@
+import { Factory } from "./factoryMedias.js";
+
 export class Media {
     constructor(media) {
         this.title = media.title;
@@ -21,7 +23,8 @@ export class Media {
 
         const buttonOpenMedia = document.createElement('button');
         buttonOpenMedia.setAttribute('class', 'button-media');
-        
+
+        // Création d'une instance de la class Factory
         let elMedia = new Factory(this.mediaPath, this.title, this.image ? 'image' : 'video');
         
         elMedia = elMedia.create();
@@ -72,58 +75,5 @@ export class Media {
                 span.appendChild(icon);
             })
         }
-    }
-}
-
-class Factory {
-    constructor(path, title, type) {
-        switch(type) {
-            case 'image': 
-                return new Image(path, title);
-            case 'video':
-                return new Video(path, title);
-        }
-    }
-}
-
-class Image {
-    constructor(path, title) {
-        this.path = path;
-        this.title = title;
-    }
-
-    create() {
-        this.element = document.createElement('img');
-        this.element.setAttribute('alt', this.title);
-        this.element.setAttribute('src', this.path);
-        this.element.setAttribute('class', 'image-media');
-        this.element.setAttribute('aria-haspopup', 'dialog');
-        this.element.setAttribute('aria-controls', 'carousel_modal');
-        this.element.setAttribute('data-name', `${this.title}`);
-        return this.element;
-    }
-}
-
-class Video{
-    constructor(path, title) {
-        this.path = path;
-        this.title = title;
-    }
-
-    create() {
-        this.element = document.createElement('video');
-        this.element.setAttribute('alt', this.title);
-
-        this.error = document.createElement('span');
-        this.error.textContent = 'Votre navigateur ne prend pas en charge les vidéos HTML5';
-
-        this.element.appendChild(this.error);
-
-        this.element.setAttribute('src', this.path);
-        this.element.setAttribute('class', 'image-media');
-        this.element.setAttribute('aria-haspopup', 'dialog');
-        this.element.setAttribute('aria-controls', 'carousel_modal');
-        this.element.setAttribute('data-name', `${this.title}`);
-        return this.element;
     }
 }
